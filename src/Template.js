4a2f933acc0a684e0f6e9fad0bc992ce5eee8b05
Shoaib Burq <saburq@gmail.com>
2017-06-11 16:02:24 +0200

    make path to style relative

--- src/Template.js
+++ src/Template.js
@@ -6,12 +6,12 @@ export default class Template extends React.Component {
     return(
       <html>
         <head>
-          <link rel="stylesheet" href="/styles.css"/>
+          <link rel="stylesheet" href="./styles.css"/>
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
           <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" crossorigin="anonymous"></script>
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
-          <link rel="icon" href="/favicon.ico"/>
+          <link rel="icon" href="./favicon.ico"/>
         </head>
         <body>
           <div id='outlet'>
